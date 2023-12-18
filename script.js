2d936a195df8e3a33c15e72ca4995464829c5f0d
MossaCobra <jaydengalea11@gmail.com>
2023-12-18 17:46:17 +1100

    Adding function

--- script.js
+++ script.js
@@ -12,5 +12,15 @@ function generateRandomNumber(num) {
 
   for(let prop in collectiveWisdom){
     let optionIdx = generateRandomNumber(collectiveWisdom[prop].length);
-  }
 
+    switch(prop){
+        case 'signInfo':
+            personalWisdom.push(generateSignInfo(collectiveWisdom[prop]));
+            break;
+        case 'fortuneOutput':
+            personalWisdom.push(generateFortuneOutput(collectiveWisdom[prop]));
+            break;
+        case 'advice':
+            personalWisdom.push(generateRandomNumber(collectiveWisdom[prop])); 
+    }
+  }
